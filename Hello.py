52af985e00e193fb1d94256badc582d5b6bd96a5
althoughh <95028240+althoughh@users.noreply.github.com>
2023-11-28 01:17:06 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,6 @@ pip install st-card-component
 from st_card_component import st_card
 
 # Example usage
-st_card(title="Card Title", text="This
 
 def run():
     st.sidebar.success("Select some options.")
@@ -14,15 +13,43 @@ def run():
     jtbd_df = pd.read_csv('jtbd.csv')
     role_df = pd.read_csv('role.csv')
 
-    # Sidebar Dropdown for Industry selection
-    selected_industry = st.sidebar.selectbox("Select an Industry", options=industry_df['Industry'].unique())
-    industry_info = industry_df[industry_df['Industry'] == selected_industry]
-    if not industry_info.empty:
-        for column in industry_info.columns:
-            with st.container():
-                st.markdown(f"**{column}**")
-                st.text_area("", industry_info.iloc[0][column], height=100, key=f"industry_{column}")
 
+    # Sidebar Dropdown for Industry selection
+    selected_industry = st.sidebar.selectbox("Select an Industry", options=[''] + list(industry_df['Industry'].unique()))
+
+    if selected_industry:
+        industry_info = industry_df[industry_df['Industry'] == selected_industry]
+
+        # Define the groups for the cards
+        groups = [
+            ["Industry Overview", "Industry-Specific Needs", "Regulatory Environment"],
+            ["Common Roles Involved in Hiring Process", "Challenges and Concerns", "Key Drivers for Background Checks"],
+            ["Decision Influencers", "Preferred Features in a Solution", "Impact on Operations"],
+            ["Messaging and Communication"]
+        ]
+
+        # Injecting Bootstrap CSS
+        st.markdown("""
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        """, unsafe_allow_html=True)
+
+        # Display the cards in a grid layout
+        for group in groups:
+            with st.container():
+                for field in group:
+                    content = industry_info[field].values[0] if field in industry_info else "No information"
+                    st.markdown(get_card_html(field, content), unsafe_allow_html=True)
+
+def get_card_html(title, content):
+    return f"""
+        <div class="card bg-light mb-3" style="max-width: 18rem;">
+            <div class="card-header">{title}</div>
+            <div class="card-body">
+                <p class="card-text">{content}</p>
+            </div>
+        </div>
+    """
+   
     # Filter roles and jtbd based on the selected industry
     filtered_role = role_df[role_df['Industry'] == selected_industry]
 
