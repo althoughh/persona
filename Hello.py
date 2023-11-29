0e88d92487a591f62283ba3a2be4e2f577c54133
althoughh <95028240+althoughh@users.noreply.github.com>
2023-11-29 17:56:47 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -63,11 +63,12 @@ def display_info_with_cards(df, section):
 def get_bootstrap_card_html(title, content, card_color, num_columns):
     card_width = "100%"  # Adjust the width to 100% of the column
     return f"""
-        <div class="card border-{card_color} mb-2" style="width: {card_width}; margin: 2px; padding: 5px;">
-            <h4 class="card-title" style="color: {card_color};">{title}</h4>
-            <p class="card-text" style="font-size: 0.9em;">{content}</p>
+        <div style="width: {card_width}; margin: 2px; padding: 5px; border: 1px solid {card_color}; border-radius: 5px;">
+            <h4 style="color: {card_color};">{title}</h4>
+            <p style="font-size: 0.9em;">{content}</p>
         </div>
     """
+
 
 def show_content_ideas(content_ideas):
     if content_ideas.empty:
