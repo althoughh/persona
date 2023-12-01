c47b2423cdeb7fbaa62c784bbaa64d5c10d4a5f4
althoughh <95028240+althoughh@users.noreply.github.com>
2023-12-01 11:48:25 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -21,6 +21,13 @@ df = conn.read()
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
+
+df = conn.read(
+    worksheet="Sheet1",
+    ttl="10m",
+    usecols=[0, 1],
+    nrows=3,
+)
 # Define custom styles for the main page and sidebar
 main_style = """
 <style>
